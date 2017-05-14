f467549f30db42fc70f111bda18af52dfb502f7b
Vu Toan <vutoan@Vus-MacBook-Pro.lan>
2017-05-14 09:38:41 +0700

    first commit

---
+++ src/App1.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Editor, RichUtils, EditorState } from 'draft-js'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state={ editorState: EditorState.createEmpty() }
+    this.onChange=(editorState) => { this.setState({ editorState }) }
+    this.handleKeyCommand = this.handleKeyCommand.bind(this)
+  }
+
+  handleKeyCommand(command) {
+    console.log(command)
+    const newState = RichUtils.handleKeyCommand(this.state.EditorState, command)
+    if(newState) {
+      this.onChange(newState)
+      return 'handled'
+    }
+    return 'not-handled'
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Editor
+          onChange={ this.onChange }
+          handleKeyCommand={this.handleKeyCommand}
+          editorState={ this.state.editorState } />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
