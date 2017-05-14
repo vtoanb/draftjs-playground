b17dacc1352ad3fff5360db9b62f61c5180133cd
Vu Toan <vutoan@Vus-MacBook-Pro.lan>
2017-05-14 10:03:17 +0700

    familiar with inlinestyle and keycommand

--- src/App.js
+++ src/App.js
@@ -1,31 +1,57 @@
-import React from 'react'
-import {Editor, EditorState, RichUtils} from 'draft-js';
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Editor, RichUtils, EditorState } from 'draft-js'
 
-class App extends React.Component {
+class App extends Component {
   constructor(props) {
-    super(props);
-    this.state = {editorState: EditorState.createEmpty()};
-    this.onChange = (editorState) => this.setState({editorState});
-    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+    super(props)
+    this.state={ editorState: EditorState.createEmpty() }
+    this.onChange=(editorState) => { this.setState({ editorState }) }
+    this.handleKeyCommand = this.handleKeyCommand.bind(this)
+    this._onBoldClick = this._onBoldClick.bind(this)
+    this._onItalicClick = this._onItalicClick.bind(this)
   }
+
   handleKeyCommand(command) {
-    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
-    if (newState) {
-      this.onChange(newState);
-      return 'handled';
+    console.log(command)
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command)
+    if(newState) {
+      this.onChange(newState)
+      return 'handled'
     }
-    return 'not-handled';
+    return 'not-handled'
   }
+
+  _onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'))
+  }
+
+  _onItalicClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'))
+  }
+
   render() {
     return (
-      <Editor
-        editorState={this.state.editorState}
-        handleKeyCommand={this.handleKeyCommand}
-        onChange={this.onChange}
-      />
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Editor
+          onChange={ this.onChange }
+          handleKeyCommand={this.handleKeyCommand}
+          editorState={ this.state.editorState } />
+        <button onClick={this._onBoldClick}>BOLD</button>
+        <button onClick={this._onItalicClick}>ITALIC</button>
+      </div>
     );
   }
 }
 
-export default App
+export default App;
+
 
